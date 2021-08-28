69bca99005f5140cc10301dfdf702d9b5d770bf8
Esteban Obilinovic <e.obilinovic@gmail.com>
2021-08-27 23:43:34 -0400

    13-serving-static-files-with-express

---
+++ 13-serving-static-files-with-express.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use("/public",express.static(path.join(__dirname,"static")));
+
+
+app.get("/",(req,res) =>{
+    res.sendFile(path.join(__dirname,"static","index.html"));
+});
+
+app.get("/example",(req,res) =>{
+    res.send("hitting example route");
+});
+
+app.get("/example/:name/:age",(req,res) =>{
+    console.log(req.params);
+    console.log(req.query);
+    res.send(req.params.name + " : " + req.params.age);
+});
+
+app.listen(3000);
